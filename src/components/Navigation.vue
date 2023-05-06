be832c24ce2e1de100192b1879e66c88a36c5776
nad279444 <nardfrimpong@gmail.com>
2023-05-06 12:52:23 +0000

    Created the styling for the  nav bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,11 @@
 <template>
-  <header>
-    <h1> Hello </h1>
+  <header class="bg-at-light-green text-white">
+    <nav class="container py-5 px-4 flex flex-col sm:flex-row gap-4 items-center">
+      <div class="flex items-center gap-x-4">
+        <img  class="w-14" src="../assets/images/dumbbell-light.png"/>
+        <h1 class="text-lg">Ben's Workout Tracker</h1>
+      </div>
+    </nav>
   </header>
 </template>
 
