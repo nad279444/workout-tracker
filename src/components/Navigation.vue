688c94296c095860a43c2179d2c0d8d8d4882543
nad279444 <nardfrimpong@gmail.com>
2023-05-08 15:40:08 +0000

    Created a logout page and restricted pages for for unauthenticated users

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,24 +7,29 @@
       </div>
       <ul class="flex flex-1 justify-end gap-x-10">
         <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
-        <router-link class="cursor-pointer" :to="{ name: '' }">Create</router-link>
-        <router-link  class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
-        <li class="cursor-pointer">Logout</li>
+        <router-link  v-if="user" class="cursor-pointer" :to="{ name: '' }">Create</router-link>
+        <router-link  v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
+        <li @click="logOut" v-if="user" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
 </template>
 
-<script>
-export default {
-  setup() {
-    // Get user from store
+<script setup>
+import { supabase } from '../supabase/init';
+import { useRouter } from 'vue-router';
+import store from '../store/index';
+import { computed } from 'vue';
 
-    // Setup ref to router
+ // Setup ref to router
+ const router = useRouter()
 
-    // Logout function
 
-    return {};
-  },
-};
+ const user = computed(() => store.state.user)
+
+const logOut = async () => {
+    await supabase.auth.signOut()
+    router.push({name: "Home"})
+}
+
 </script>
