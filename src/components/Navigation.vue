a17f1788474bee419571525d7bf8917f3e27797c
nad279444 <nardfrimpong@gmail.com>
2023-05-09 17:27:09 +0000

    created the interface for create page and added functionality for adding exercises

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,3 @@
-<template>
-  <header class="bg-at-light-green text-white">
-    <nav class="container py-5 px-4 flex flex-col sm:flex-row gap-4 items-center">
-      <div class="flex items-center gap-x-4">
-        <img  class="w-14" src="../assets/images/dumbbell-light.png"/>
-        <h1 class="text-lg">Ben's Workout Tracker</h1>
-      </div>
-      <ul class="flex flex-1 justify-end gap-x-10">
-        <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
-        <router-link  v-if="user" class="cursor-pointer" :to="{ name: '' }">Create</router-link>
-        <router-link  v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
-        <li @click="logOut" v-if="user" class="cursor-pointer">Logout</li>
-      </ul>
-    </nav>
-  </header>
-</template>
 
 <script setup>
 import { supabase } from '../supabase/init';
@@ -33,3 +17,20 @@ const logOut = async () => {
 }
 
 </script>
+
+<template>
+  <header class="bg-at-light-green text-white">
+    <nav class="container py-5 px-4 flex flex-col sm:flex-row gap-4 items-center">
+      <div class="flex items-center gap-x-4">
+        <img  class="w-14" src="../assets/images/dumbbell-light.png"/>
+        <h1 class="text-lg">Ben's Workout Tracker</h1>
+      </div>
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }">Home</router-link>
+        <router-link  v-if="user" class="cursor-pointer" :to="{ name: 'Create' }">Create</router-link>
+        <router-link  v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }">Login</router-link>
+        <li @click="logOut" v-if="user" class="cursor-pointer">Logout</li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue"
 import  Register from "../views/Register.vue"
+import Create from "../views/Create.vue"
 
 const routes = [
   {
@@ -18,7 +19,12 @@ const routes = [
     path: "/Register",
     name: "Register",
     component: Register,
- }
+ },
+ {
+  path: "/Create",
+  name: "Create",
+  component: Create,
+}
 ];
 
 const router = createRouter({
